8b24025059ba3534a488ef03e6892ef73f47922a
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-21 16:08:38 +0300

    Create methods of this class

--- src/Lab2/GiftSetInfo.java
+++ src/Lab2/GiftSetInfo.java
@@ -6,5 +6,39 @@ public class GiftSetInfo {
     private double chocolatesWeight;
     private double wafflesWeight;
     private double drageeWeight;
+    private double totalPrice;
+
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public void setLollipopsWeight(double weight, double price){
+        this.lollipopsWeight=weight;
+        this.totalPrice+=weight*price;
+    }
+
+    public void setChocolatesWeight(double weight, double price){
+        this.chocolatesWeight=weight;
+        this.totalPrice+=weight*price;
+    }
+
+    public void setWafflesWeight(double weight, double price){
+        this.wafflesWeight=weight;
+        this.totalPrice+=weight*price;
+    }
+
+    public void setDrageeWeight(double weight, double price){
+        this.drageeWeight=weight;
+        this.totalPrice+=weight*price;
+    }
+
+    public void outputInfo(){
+        System.out.println("Назва подарункового набору: "+name+"\n" +
+                "Льодяники: "+lollipopsWeight+" кг\n" +
+                "Шоколадні цукерки: "+chocolatesWeight+" кг\n" +
+                "Вафлі: "+wafflesWeight+" кг\n" +
+                "Драже: "+drageeWeight+" кг\n" +
+                "Загальна вартість: "+totalPrice+" грн\n");
+    }
 
 }
