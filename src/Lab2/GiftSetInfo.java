b585a6b7bbe8579858bd2d88f51fca41d0df6e86
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-21 15:44:36 +0300

    Add fields into GiftSetInfo

---
+++ src/Lab2/GiftSetInfo.java
@@ -0,0 +1,10 @@
+package Lab2;
+
+public class GiftSetInfo {
+    private String name;
+    private double lollipopsWeight;
+    private double chocolatesWeight;
+    private double wafflesWeight;
+    private double drageeWeight;
+
+}
