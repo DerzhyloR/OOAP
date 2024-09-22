db063821302077837dc74048a94107514f4f4443
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-22 15:48:57 +0300

    Creating Buying class

--- src/Lab2_1/Buying.java
+++ src/Lab2_1/Buying.java
@@ -1,5 +1,26 @@
 package Lab2_1;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Buying {
+    Map<String,Facade> ordering=new HashMap<>();
+    String materials[]={"Плівковий","Фарбований","Пластиковий"};
+
+    public void addNewFacade(Facade facade){
+        ordering.put(facade.getMaterial(),facade);
+    }
+
+    public void outputOrdering(){
+        for(int i=0;i<materials.length;i++) {
+            for (Map.Entry<String,Facade>facade:ordering.entrySet()) {
+
+            }
+        }
+    }
+
+
 
 }
