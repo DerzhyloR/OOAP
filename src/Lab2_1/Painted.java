0f6dd5cbef0e1bfc79cfe44e0e76452d798164ba
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-22 18:25:23 +0300

    Lower prices

--- src/Lab2_1/Painted.java
+++ src/Lab2_1/Painted.java
@@ -3,11 +3,11 @@ package Lab2_1;
 public class Painted implements FacadeFactory{
     @Override
     public Facade solidFacade(double width, double height) {
-        return new Solid(width,height,120,"Фарбований");
+        return new Solid(width,height,100,"Фарбований");
     }
 
     @Override
     public Facade storeFrontFacade(double width, double height) {
-        return new StoreFront(width,height,120,"Фарбований");
+        return new StoreFront(width,height,100,"Фарбований");
     }
 }
--- src/Lab2_1/Plastic.java
+++ src/Lab2_1/Plastic.java
@@ -3,11 +3,11 @@ package Lab2_1;
 public class Plastic implements FacadeFactory{
     @Override
     public Facade solidFacade(double width, double height) {
-        return new Solid(width,height,90,"Пластиковий");
+        return new Solid(width,height,70,"Пластиковий");
     }
 
     @Override
     public Facade storeFrontFacade(double width, double height) {
-        return new StoreFront(width,height,90,"Пластиковий");
+        return new StoreFront(width,height,70,"Пластиковий");
     }
 }
