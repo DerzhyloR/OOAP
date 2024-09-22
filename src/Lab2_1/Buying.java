441bd240d9c9cb16649eec98499bf7c989a309be
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-22 16:25:14 +0300

    Creating method for output information about facade

--- src/Lab2_1/Buying.java
+++ src/Lab2_1/Buying.java
@@ -1,8 +1,6 @@
 package Lab2_1;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Buying {
@@ -15,8 +13,19 @@ public class Buying {
 
     public void outputOrdering(){
         for(int i=0;i<materials.length;i++) {
+            int kilkist=0;
+            double totalPrice=0;
             for (Map.Entry<String,Facade>facade:ordering.entrySet()) {
-
+                if(facade.getValue().getMaterial().equals(materials[i])){
+                    System.out.println("Тип фасаду: "+facade.getValue().getTypeFacade()+"\tРозмір: "+facade.getValue().getSize()+" кв.м"+
+                            "\tМатеріал:"+facade.getValue().getMaterial()+"\tЦіна: "+facade.getValue().getPrice()+" грн");
+                    kilkist++;
+                    totalPrice+=facade.getValue().getPrice();
+                }
+            }
+            if(kilkist!=0){
+                System.out.println("Загальна ціна замовдення: "+totalPrice+" грн");
+                System.out.println("---------------------------------------------------");
             }
         }
     }
