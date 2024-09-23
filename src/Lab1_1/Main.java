ce032bcf36719553f39837b9de700352bf8d7b4a
Roman <23derzhylo.r@nltu.lviv.ua>
2024-09-23 23:23:51 +0300

    Adding error checking method

--- src/Lab1_1/Main.java
+++ src/Lab1_1/Main.java
@@ -1,15 +1,41 @@
 package Lab1_1;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
     public static void main(String [] args){
-        Figure square= new Square(12);
+        double sideSquare=getDataDouble("Введіть сторону квадрата: ");
+        Figure square= new Square(sideSquare);
         square.areaOfTheFigure();
         square.outputArea();
-        Figure rectangle= new Rectangle(5,10);
+        double length=getDataDouble("Введіть довжину прямокутника: ");
+        double width=getDataDouble("Введіть ширину прямокутника: ");
+        Figure rectangle= new Rectangle(length,width);
         rectangle.areaOfTheFigure();
         rectangle.outputArea();
-        Figure circle= new Circle(8);
+        double radius=getDataDouble("Введіть радіус круга: ");
+        Figure circle= new Circle(radius);
         circle.areaOfTheFigure();
         circle.outputArea();
+    }
+
+    public static double getDataDouble(String txt){
+        while (true){
+            try {
+                System.out.println(txt);
+                double chyslo=scanner.nextDouble();
+                scanner.nextLine();
+                if(chyslo>0){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
