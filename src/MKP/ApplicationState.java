fb31b378802cd61c97f595caacf5f14eaa5ba1cb
Roman <23derzhylo.r@nltu.lviv.ua>
2024-10-09 15:21:35 +0300

    Creating constructor and methods for class

--- src/MKP/ApplicationState.java
+++ src/MKP/ApplicationState.java
@@ -3,6 +3,27 @@ package MKP;
 public class ApplicationState {
     private static ApplicationState instance;  //Зберігатиме екземпляр класу
     public String LoginId;
-    public int MaxSize;
+    public double MaxSize;
+
+    public ApplicationState(){
+        LoginId="Невідомо";
+        MaxSize=0;
+        instance=null;
+    }
+
+    public void setLoginId(String value){
+        LoginId=value;
+    }
+    public void setMaxSize(double size){
+        MaxSize=size;
+    }
+
+    public String getLoginId(){
+        return LoginId;
+    }
+
+    public double getMaxSize(){
+        return MaxSize;
+    }
 
 }
