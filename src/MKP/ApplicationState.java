061ce5d3440157940e61e45783c5dc0e16fd87fd
Roman <23derzhylo.r@nltu.lviv.ua>
2024-10-09 15:16:03 +0300

    Adding fields to class

--- src/MKP/ApplicationState.java
+++ src/MKP/ApplicationState.java
@@ -1,4 +1,8 @@
 package MKP;
 
 public class ApplicationState {
+    private static ApplicationState instance;  //Зберігатиме екземпляр класу
+    public String LoginId;
+    public int MaxSize;
+
 }
