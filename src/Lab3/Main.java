b76b78ae464ba719d6171d7e8b85b14cc2ba80a5
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 21:09:42 +0200

    reating method for check double number

--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -1,10 +1,32 @@
 package Lab3;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
     public static void main(String [] args){
         ArrayList<Object> data= new ArrayList<>();
 
+
+    }
+
+    public static int getDataInt(String txt){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("Помилка вибору! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
