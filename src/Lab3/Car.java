684f68ceb64a51de2b21d53c1a1b8813ace73294
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:10:22 +0200

    Add constructor and methods

--- src/Lab3/Car.java
+++ src/Lab3/Car.java
@@ -1,5 +1,22 @@
 package Lab3;
 
 abstract class Car {
+    protected String clientTypeOfPayment;
+    protected String model;
+    protected String typeOfInsurance;
+    protected String guarantee;
+    protected double price;
+
+    public Car(String clientTypeOfPayment,String model, double price){
+        this.clientTypeOfPayment=clientTypeOfPayment;
+        this.model=model;
+        this.price=price;
+        typeOfInsurance="Unknown";
+        guarantee="Unknown";
+    }
+
+    public abstract double finalPrice();
+    public abstract String setGuarantee();
+    public abstract String setTypeOfInsurance();
 
 }
