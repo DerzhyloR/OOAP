ac403ccbcbf5e2e6bbe21237a27797f39543f276
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:43:19 +0200

    Creating methods for InstallmentsPayment class

--- src/Lab3/InstallmentsPayment.java
+++ src/Lab3/InstallmentsPayment.java
@@ -8,16 +8,17 @@ public class InstallmentsPayment extends Car {
 
     @Override
     public void finalPrice() {
-
+        double newPrice=price*1.2;
+        price=newPrice;
     }
 
     @Override
     public void setGuarantee() {
-
+        guarantee="3 місяців";
     }
 
     @Override
     public void setTypeOfInsurance() {
-
+        typeOfInsurance="-10% знижки на загальну суму ремонту";
     }
 }
