cb5c9767ae2cfefe06835f9f081e430c8ec973ba
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:37:10 +0200

    Creating class for installments payment

---
+++ src/Lab3/InstallmentsPayment.java
@@ -0,0 +1,18 @@
+package Lab3;
+
+public class InstallmentsPayment extends Car {
+    @Override
+    public void finalPrice() {
+
+    }
+
+    @Override
+    public void setGuarantee() {
+
+    }
+
+    @Override
+    public void setTypeOfInsurance() {
+
+    }
+}
