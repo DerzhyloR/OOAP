8cca60554c97268a8e457f4a23c577ca8bb6c17e
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:28:28 +0200

    Creating class for credit payment

---
+++ src/Lab3/CreditPayment.java
@@ -0,0 +1,18 @@
+package Lab3;
+
+public class CreditPayment extends Car{
+    @Override
+    public void finalPrice() {
+
+    }
+
+    @Override
+    public void setGuarantee() {
+
+    }
+
+    @Override
+    public void setTypeOfInsurance() {
+
+    }
+}
