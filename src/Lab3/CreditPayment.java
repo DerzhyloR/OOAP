8a8974acc7f47081f8b28b26c733734b753b8345
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:33:11 +0200

    Creating methods for CreditPayment class

--- src/Lab3/CreditPayment.java
+++ src/Lab3/CreditPayment.java
@@ -8,16 +8,17 @@ public class CreditPayment extends Car{
 
     @Override
     public void finalPrice() {
-
+        double newPrice=price*1.5;
+        price=newPrice;
     }
 
     @Override
     public void setGuarantee() {
-
+        guarantee="8 місяців";
     }
 
     @Override
     public void setTypeOfInsurance() {
-
+        typeOfInsurance="-20% знижки на загальну суму ремонту";
     }
 }
