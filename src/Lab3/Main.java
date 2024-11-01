bb5cfeb1c518e74cc0b0e499d72cf236a4ee3ca0
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 22:04:23 +0200

    add information for input data

--- src/Lab3/Main.java
+++ src/Lab3/Main.java
@@ -1,17 +1,42 @@
 package Lab3;
 
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
     private static Scanner scanner= new Scanner(System.in);
     public static void main(String [] args){
-        ArrayList<Object> data= new ArrayList<>();
+        Map<Client,Car> data= new HashMap<>();
         System.out.println("Введіть ім'я клієнта: ");
         String name=scanner.nextLine();
+        int age=getDataInt("Введіть вік клієнта:",false);
+        System.out.println("Введіть стать клієнта: ");
+        String sex=scanner.nextLine();
+        Client client= new Client(name,age,sex);
+        System.out.println("Введіть марку автомобіля: ");
+        String model=scanner.nextLine();
+        double price=getDataDouble("Введіть ціну автомобіля: ");
+        int chooseTypeOfPayment=getDataInt("Виберіть тип оплати за машину:\n1.Повна оплата\n2.Кредитом\n3.У розстрочку\nВаш вибір: ",true);
+        String typePayment=findoutTypePayment(chooseTypeOfPayment);
+        Car car=null;
+        car=car.creatingObject(chooseTypeOfPayment,typePayment,model,price);
+        data.put(client,car);
 
+    }
 
-
+    public static String findoutTypePayment(int number){
+        String res="";
+        if(number==1){
+            res="Повна оплата";
+        }
+        else if(number==2){
+            res="Кредитом";
+        }
+        else {
+            res="У розстрочку";
+        }
+        return res;
     }
 
     public static double getDataDouble(String txt){
