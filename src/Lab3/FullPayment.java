9a7287f826d7b3f2f41fca6f56f1b2023861adb9
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:26:30 +0200

    Creating methods

--- src/Lab3/FullPayment.java
+++ src/Lab3/FullPayment.java
@@ -7,16 +7,17 @@ public class FullPayment extends Car{
 
     @Override
     public void finalPrice() {
-
+        double newPrice=price*0.95;
+        price=newPrice;
     }
 
     @Override
     public void setGuarantee() {
-
+        guarantee="12 місяців";
     }
 
     @Override
     public void setTypeOfInsurance() {
-
+        typeOfInsurance="-50% знижки на загальну суму ремонту";
     }
 }
