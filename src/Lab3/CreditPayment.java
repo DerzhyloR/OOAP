f10c923b791beba2d885be0919b06c02dde00a60
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:29:37 +0200

    Creating constructor for CreditPayment class

--- src/Lab3/CreditPayment.java
+++ src/Lab3/CreditPayment.java
@@ -1,6 +1,11 @@
 package Lab3;
 
 public class CreditPayment extends Car{
+
+    public CreditPayment(String clientTypeOfPayment,String model, double price){
+        super(clientTypeOfPayment,model,price);
+    }
+
     @Override
     public void finalPrice() {
 
