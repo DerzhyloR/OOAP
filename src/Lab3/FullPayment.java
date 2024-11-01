68a1789675cfd69bc7ff909cefa81d66bafbe853
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:21:00 +0200

    Creating construct in fullPayment class

--- src/Lab3/FullPayment.java
+++ src/Lab3/FullPayment.java
@@ -1,6 +1,9 @@
 package Lab3;
 
 public class FullPayment extends Car{
+    public FullPayment(String clientTypeOfPayment,String model, double price){
+        super(clientTypeOfPayment,model,price);
+    }
 
     @Override
     public void finalPrice() {
