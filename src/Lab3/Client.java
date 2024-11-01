dca82bfae1874c992e4b42166adb6ea441104c49
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-01 19:48:21 +0200

    Creating Claint class, fields and constructor

---
+++ src/Lab3/Client.java
@@ -0,0 +1,14 @@
+package Lab3;
+
+public class Client {
+    private String name;
+    private int age;
+    private String sex;
+
+    public Client(String name, int age, String sex){
+        this.name=name;
+        this.age=age;
+        this.sex=sex;
+    }
+
+}
