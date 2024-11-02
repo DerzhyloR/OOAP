706af6fcf6fadc974811130a6ec5285ee1c8354d
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-02 13:37:26 +0200

    Add method for get final price

--- src/Lab3_1/SalesProduct.java
+++ src/Lab3_1/SalesProduct.java
@@ -1,15 +1,42 @@
 package Lab3_1;
 
+import java.util.Random;
+
 public class SalesProduct {
     private static final SalesProduct sales= new SalesProduct();
     private int countFood;
     private int countClothes;
     private int countMedicine;
+    private double totalPrice;
 
     private SalesProduct(){
         countClothes=0;
         countFood=0;
         countMedicine=0;
+        totalPrice=0;
+    }
+
+    public SalesProduct getObject(){
+        return sales;
+    }
+
+    private double finalPrice(int number){
+        Random rand = new Random();
+        double firsPrice=50 + (rand.nextDouble() * 500);
+        double price=0;
+        switch (number){
+            case 1:
+                price=firsPrice*1.15;
+                break;
+            case 2:
+                price=firsPrice*1.05;
+                break;
+            case 3:
+                price=firsPrice*1.10;
+                break;
+        }
+        totalPrice+=price;
+        return price;
     }
 
 
