8335a315600238de4db4f6fef88cdebb39d5e990
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-02 14:51:08 +0200

    Actions with the object

--- src/Lab3_1/Main.java
+++ src/Lab3_1/Main.java
@@ -1,7 +1,35 @@
 package Lab3_1;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
     public static void main(String [] args){
         SalesProduct sales=SalesProduct.getObject();
+        sales.clearFileOrCreate();
+        while (true) {
+            int number = getDataInt("Виберіть який продукт продати:\n1.Одяг\n2.Їжа\n3.Ліки\nВаш вибір: ");
+            sales.finalPrice(number);
+        }
+
+    }
+
+    public static int getDataInt(String txt){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("ПОМИЛКА ВИБОРУ! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
