852ca13a6b208d17ab790e8a78e53b91949f3090
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-02 13:20:15 +0200

    Add final object, fields and constructor

--- src/Lab3_1/SalesProduct.java
+++ src/Lab3_1/SalesProduct.java
@@ -1,4 +1,16 @@
 package Lab3_1;
 
 public class SalesProduct {
+    private static final SalesProduct sales= new SalesProduct();
+    private int countFood;
+    private int countClothes;
+    private int countMedicine;
+
+    private SalesProduct(){
+        countClothes=0;
+        countFood=0;
+        countMedicine=0;
+    }
+
+
 }
