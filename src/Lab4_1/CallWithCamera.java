818c29478c9e497cef589bf54413cef31844a5fb
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-09 19:00:23 +0200

    Create class for call with camera using Phone interface

---
+++ src/Lab4_1/CallWithCamera.java
@@ -0,0 +1,10 @@
+package Lab4_1;
+
+public class CallWithCamera implements Phone{
+    private VideoCamera videoCall;
+
+    public CallWithCamera(VideoCamera videoCall){
+        this.videoCall=videoCall;
+    }
+
+}
