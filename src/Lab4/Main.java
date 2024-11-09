6ee8ba32913a0b3b917fbd937bd7c77775586c29
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-09 18:15:13 +0200

    Create firts clock

--- src/Lab4/Main.java
+++ src/Lab4/Main.java
@@ -2,6 +2,8 @@ package Lab4;
 
 public class Main {
     public static void main(String [] args){
+        AlarmClockActions alertClock1= new FirstAlarmClockAlert();
+        AlarmClock clock1=new CreateAlarmClock(alertClock1);
 
     }
 }
