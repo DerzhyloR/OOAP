e85d6393ad4f6ebab4188de27fe4bf6dd8a1624e
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-09 19:05:41 +0200

    Add method for check integer number

--- src/Lab4_1/Main.java
+++ src/Lab4_1/Main.java
@@ -1,7 +1,29 @@
 package Lab4_1;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
     public static void main(String [] args){
 
+    }
+
+    public static int getDataInt(String txt){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0 && chyslo<3){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("ПОМИЛКА ВИБОРУ! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
