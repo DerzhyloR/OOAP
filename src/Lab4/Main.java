6b571d42872db0e0592dd77b2217c830b7dc4c04
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-09 18:16:19 +0200

    Call methods for first clock

--- src/Lab4/Main.java
+++ src/Lab4/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String [] args){
         AlarmClockActions alertClock1= new FirstAlarmClockAlert();
         AlarmClock clock1=new CreateAlarmClock(alertClock1);
-
+        clock1.start();
+        clock1.toWake();
+        clock1.stop();
+        System.out.println();
     }
 }
