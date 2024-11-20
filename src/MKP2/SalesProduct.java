4b3e6e2078de084c301f53270cc379e73a67dd2f
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-20 19:50:15 +0200

    Creating final object and other fields for program

--- src/MKP2/SalesProduct.java
+++ src/MKP2/SalesProduct.java
@@ -1,4 +1,9 @@
 package MKP2;
 
 public class SalesProduct {
+    private static final SalesProduct sales= new SalesProduct();
+    private int countFood;
+    private int countClothes;
+    private int countMedicine;
+    private double totalPrice;
 }
