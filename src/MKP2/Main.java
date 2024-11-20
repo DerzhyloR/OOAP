099f8deaba870b822097f053e9895cd230b18e08
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-20 20:14:42 +0200

    Add method for check on error for integer numbers

--- src/MKP2/Main.java
+++ src/MKP2/Main.java
@@ -1,8 +1,31 @@
 package MKP2;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner= new Scanner(System.in);
+
     public static void main(String [] args){
         SalesProduct sales=SalesProduct.getObject();
         sales.clearFileOrCreate();
+    }
+
+    public static int getDataInt(String txt){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0 && chyslo<4){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("ПОМИЛКА ВИБОРУ! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
