b8ada06f7c13672f8bcff2ce17b2bb3f6662713d
Roman <23derzhylo.r@nltu.lviv.ua>
2024-11-20 20:10:28 +0200

    Get object of SalesProduct class

--- src/MKP2/Main.java
+++ src/MKP2/Main.java
@@ -2,6 +2,7 @@ package MKP2;
 
 public class Main {
     public static void main(String [] args){
-
+        SalesProduct sales=SalesProduct.getObject();
+        sales.clearFileOrCreate();
     }
 }
