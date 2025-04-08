a8432c1aae7184aeffb93acbc93745f2d671c89a
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 22:28:59 +0300

    adding methods in map class

--- src/Lab6/MapComponent.java
+++ src/Lab6/MapComponent.java
@@ -1,4 +1,30 @@
 package Lab6;
 
-public class MapComponent {
+public abstract  class MapComponent {
+    protected MapComponent parent;
+    protected String name;
+    protected int _x, _y;
+
+    public MapComponent(String name) {
+        this.name = name;
+    }
+
+    public void setParent(MapComponent parent) {
+        this.parent = parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract void draw(int x, int y);
+
+    public MapComponent findChild(String name) {
+        if (this.name.equals(name)) {
+            return this;
+        }
+        return null;
+    }
+
+
 }
