2d0b4d06da06eb10def8e63a06cad774501f00d1
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 19:47:05 +0300

    initialization orc class

--- src/Lab5/Orc.java
+++ src/Lab5/Orc.java
@@ -1,4 +1,16 @@
 package Lab5;
 
 public class Orc extends Character{
+    public Orc(){
+        super("Орк",40,35);
+    }
+    @Override
+    public int getStrength() {
+        return initialStrength;
+    }
+
+    @Override
+    public int getDefense() {
+        return initialDefense;
+    }
 }
