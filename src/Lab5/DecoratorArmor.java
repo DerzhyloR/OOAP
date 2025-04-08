a08ae53f64e7e9b120c43e4b7af4ab904be51d3e
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 20:04:00 +0300

    editing decorstor class for armor

--- src/Lab5/DecoratorArmor.java
+++ src/Lab5/DecoratorArmor.java
@@ -1,4 +1,22 @@
 package Lab5;
 
-public class DecoratorArmor {
+public class DecoratorArmor extends Character{
+    private Character base;
+    private int armorBonus;
+
+    public DecoratorArmor(Character base, int armorBonus) {
+        super(base.getName(), base.getStrength(), base.getDefense());
+        this.base = base;
+        this.armorBonus = armorBonus;
+    }
+
+    @Override
+    public int getStrength() {
+        return base.getStrength();
+    }
+
+    @Override
+    public int getDefense() {
+        return base.getDefense()+armorBonus;
+    }
 }
