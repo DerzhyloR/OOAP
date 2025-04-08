ca12087aaa7269e8dd7b231b6f9c5273937d135a
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 19:36:06 +0300

    add fields and methods to character class

--- src/Lab5/Character.java
+++ src/Lab5/Character.java
@@ -1,4 +1,20 @@
 package Lab5;
 
-public class Character {
+public abstract class Character {
+    protected String name;
+    protected int initialStrength;
+    protected int initialDefense;
+
+    public Character(String name, int baseStrength, int baseDefense) {
+        this.name = name;
+        this.initialStrength = baseStrength;
+        this.initialDefense = baseDefense;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract int getStrength();
+    public abstract int getDefense();
 }
