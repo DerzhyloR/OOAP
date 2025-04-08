32dc1be127b367e498e06934090a3e25c7c4f8c8
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 19:53:40 +0300

    editing decoratorweapon class

--- src/Lab5/DecoratorWeapon.java
+++ src/Lab5/DecoratorWeapon.java
@@ -1,4 +1,21 @@
 package Lab5;
 
-public class DecoratorWeapon {
+public class DecoratorWeapon extends Character{
+    private Character base;
+    private int weaponBonus;
+    public DecoratorWeapon(Character base, int weaponBonus) {
+        super(base.getName(), base.getStrength(), base.getDefense());
+        this.base = base;
+        this.weaponBonus = weaponBonus;
+    }
+
+    @Override
+    public int getStrength() {
+        return base.getStrength() + weaponBonus;
+    }
+
+    @Override
+    public int getDefense() {
+        return base.getDefense();
+    }
 }
