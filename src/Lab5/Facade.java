4f68bf71a1211b3fd6eafe89e188bcb872f517c9
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 20:12:21 +0300

    adding method for compare powers between characters

--- src/Lab5/Facade.java
+++ src/Lab5/Facade.java
@@ -1,4 +1,23 @@
 package Lab5;
 
+import java.util.List;
+
 public class Facade {
+    public void compareStrengthCharacters(Character main, List<Character> others) {
+        System.out.println("\nРІЗНИЦЯ В CИЛАХ:");
+        for (Character other : others) {
+            if (main == other) continue;
+            String result="";
+            if(main.getStrength() > other.getStrength()){
+                result="сильніший за ";
+            }
+            else if(main.getStrength() < other.getStrength()){
+                result="слабший за ";
+            }
+            else{
+                result="рівний з ";
+            }
+            System.out.println(main.getName() + " " + result + other.getName());
+        }
+    }
 }
