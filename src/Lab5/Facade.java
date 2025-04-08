105e1f608ed30d03c1ec0ec096ae0c4596bea913
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 20:20:40 +0300

    adding method for check if current character can beat others characters

--- src/Lab5/Facade.java
+++ src/Lab5/Facade.java
@@ -20,4 +20,19 @@ public class Facade {
             System.out.println(main.getName() + " " + result + other.getName());
         }
     }
+
+    public void canKills(Character attacker, List<Character> targets) {
+        System.out.println("\nМОЖЕ ПОБИТИ " + attacker.getName() + ":");
+        int attack = attacker.getStrength();
+        for (Character target : targets) {
+            if (attacker == target) continue;
+            int defense = target.getDefense();
+            for (int hits : new int[]{1, 5, 10}) {
+                if (attack * hits > defense) {
+                    System.out.println(target.getName() + " за " + hits + " удар");
+                    break;
+                }
+            }
+        }
+    }
 }
