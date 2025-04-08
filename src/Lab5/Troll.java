ae5db9b0bb69113d08d6e2d3c0179b19fe26a5a3
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 19:44:31 +0300

    initialization troll class

--- src/Lab5/Troll.java
+++ src/Lab5/Troll.java
@@ -1,4 +1,16 @@
 package Lab5;
 
 public class Troll extends Character{
+    public Troll(){
+        super("Троль",25,20);
+    }
+    @Override
+    public int getStrength() {
+        return initialStrength;
+    }
+
+    @Override
+    public int getDefense() {
+        return initialDefense;
+    }
 }
