f17ba2ff7af1f0e3f1684be7ff7c6764cc2e1e51
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-09 02:39:32 +0300

    creating class for interpreter

---
+++ src/lab8/DateInterpreter.java
@@ -0,0 +1,28 @@
+package lab8;
+
+import java.text.ParseException;
+import java.util.Date;
+
+public class DateInterpreter {
+    private DataTask dataTask;
+
+    public DateInterpreter(String format) {
+        switch (format) {
+            case "MM-DD-YYYY":
+                dataTask = new MMDDYYYYFormat();
+                break;
+            case "DD-MM-YYYY":
+                dataTask = new DDMMYYYYFormat();
+                break;
+            case "YYYY-MM-DD":
+                dataTask = new YYYYMMDDFormat();
+                break;
+            default:
+                throw new IllegalArgumentException("Невідомий формат дати!");
+        }
+    }
+
+    public Date interpret(String date) throws ParseException {
+        return dataTask.interpret(date);
+    }
+}
