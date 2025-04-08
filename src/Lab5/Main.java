0b611f84036088f726fb5a52eff777fe4a777faf
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 20:26:00 +0300

    adding method for check errors

--- src/Lab5/Main.java
+++ src/Lab5/Main.java
@@ -1,7 +1,29 @@
 package Lab5;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
+    }
+
+    public static int getDataInt(String txt,int minOptions){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0 && chyslo<=minOptions){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("Помилка вибору! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
