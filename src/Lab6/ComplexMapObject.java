8d1e09d4f6e139b8210726b52d163da6f8bb4b74
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 22:44:02 +0300

    adding algorithm in complex object class

--- src/Lab6/ComplexMapObject.java
+++ src/Lab6/ComplexMapObject.java
@@ -1,4 +1,43 @@
 package Lab6;
 
-public class ComplexMapObject {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComplexMapObject extends MapComponent  {
+    private List<MapComponent> children = new ArrayList<>();
+
+    public ComplexMapObject(String name, int x, int y) {
+        super(name);
+        this._x = x;
+        this._y = y;
+    }
+
+    public void addComponent(MapComponent component) {
+        component.setParent(this);
+        children.add(component);
+    }
+
+    @Override
+    public void draw(int x, int y) {
+        int X = x + _x;
+        int Y = y + _y;
+        System.out.println("Малювання складного об'єкту " + name + " за координатами (" + X + ", " + Y + ")");
+        for (MapComponent child : children) {
+            child.draw(X, Y);
+        }
+    }
+
+    @Override
+    public MapComponent findChild(String name) {
+        if (this.name.equals(name)) {
+            return this;
+        }
+        for (MapComponent child : children) {
+            MapComponent found = child.findChild(name);
+            if (found != null) {
+                return found;
+            }
+        }
+        return null;
+    }
 }
