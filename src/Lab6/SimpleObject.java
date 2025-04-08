f76687c38e4a78fc7f90dcaf94d2e510f12c75a5
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-08 22:37:36 +0300

    initialization class simpleobject

--- src/Lab6/SimpleObject.java
+++ src/Lab6/SimpleObject.java
@@ -1,4 +1,16 @@
 package Lab6;
 
-public class SimpleObject {
+public class SimpleObject extends MapComponent {
+    public SimpleObject(String name, int x, int y) {
+        super(name);
+        this._x = x;
+        this._y = y;
+    }
+
+    @Override
+    public void draw(int x, int y) {
+        int X = x + _x;
+        int Y = y + _y;
+        System.out.println("Намальовано " + name + " за координатами (" + X + ", " + Y + ")");
+    }
 }
