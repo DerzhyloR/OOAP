233384673e2277fde18beb78af5cb1d25125724c
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-16 23:28:26 +0300

    Adding method for enter numbers and make operation

--- src/MKP3/Main.java
+++ src/MKP3/Main.java
@@ -6,7 +6,23 @@ public class Main {
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        while (true) {
+            int operation = getDataInt("Виберіть операцію: 1.Додавання, 2.Віднімання: ",false);
+            String selectedOperation=operation==1?"Додавання":"Віднімання";
+            int num1 = getDataInt("Введіть перше ціле число: ",true);
+            int num2 = getDataInt("Введіть друге ціле число: ",true);
+            MathInterpreter mathInterpreter = new MathInterpreter(selectedOperation);
+            try {
+                int result = mathInterpreter.interpret(num1, num2);
+                System.out.println("Результат операції: " + result);
+            } catch (Exception e) {
+                System.out.println("Невірна операція!");
+            }
+            int number = getDataInt("\nВвести нові числа? 1 - так, 2 - ні: ",false);
+            if (number == 2) {
+                break;
+            }
+        }
     }
 
     public static int getDataInt(String txt, boolean isNumber) {
