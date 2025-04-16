e2bbca65a4a80da64b0c4c958954fe6f403bf7b0
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-16 23:13:11 +0300

    Override method in Substraction class

--- src/MKP3/SubtractionTask.java
+++ src/MKP3/SubtractionTask.java
@@ -1,4 +1,8 @@
 package MKP3;
 
-public class SubtractionTask extends Vuraz {
+public class SubtractionTask implements Vuraz {
+    @Override
+    public int interpret(int a, int b) {
+        return a - b;
+    }
 }
