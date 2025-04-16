f75fca49ccf95fd953a4168ca87c86a2675d3f1b
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-16 23:21:25 +0300

    Adding method for input integer number

--- src/MKP3/Main.java
+++ src/MKP3/Main.java
@@ -1,7 +1,32 @@
 package MKP3;
 
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
+    }
+
+    public static int getDataInt(String txt, boolean isNumber) {
+        while (true) {
+            try {
+                System.out.println(txt);
+                int chyslo = scanner.nextInt();
+                scanner.nextLine();
+                if (chyslo > 0 && chyslo <= 2) {
+                    return chyslo;
+                }
+                else if(isNumber){
+                    return chyslo;
+                }else {
+                    System.out.println("Помилка вибору! Повторіть спробу");
+                }
+            } catch (Exception e) {
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
     }
 }
