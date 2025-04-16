6d59229d2336cd376c7dd19dca9a1ed056dcd6fb
Roman <23derzhylo.r@nltu.lviv.ua>
2025-04-16 23:15:57 +0300

    Adding method in interpretator class

--- src/MKP3/MathInterpreter.java
+++ src/MKP3/MathInterpreter.java
@@ -1,4 +1,16 @@
 package MKP3;
 
 public class MathInterpreter {
+    private Vuraz vuraz;
+
+    public MathInterpreter(String operation) {
+        switch (operation) {
+            case "Додавання":
+                vuraz = new AdditionTask();
+                break;
+            case "Віднімання":
+                vuraz = new SubtractionTask();
+                break;
+        }
+    }
 }
