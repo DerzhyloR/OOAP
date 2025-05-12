58c5afb813731d4fc294b81ec261cbafb36b4e61
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 22:22:21 +0300

    adding method for parsing

--- src/Lab11/Parsing.java
+++ src/Lab11/Parsing.java
@@ -1,5 +1,9 @@
 package Lab11;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
@@ -13,5 +17,17 @@ public class Parsing extends RecursiveTask<List<String>> {
     @Override
     protected List<String> compute() {
         return null;
+    }
+
+    private String parsePage(String url) {
+        try {
+            Document doc = Jsoup.connect(url).get();
+            Element titleElement = doc.select("h1.product-title").first();
+            String title = titleElement != null ? titleElement.text() : "Заголовка не знайдено";
+            System.out.println("Заголовок з " + url + ": " + title);
+            return "Заголовок з " + url + ": " + title;
+        } catch (Exception e) {
+            return "ПОМИЛКА ПАРСИНГУ САЙТУ: " + url;
+        }
     }
 }
