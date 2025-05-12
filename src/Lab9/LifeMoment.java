0492dea1a4319c72b5872c0cc26b864120d60975
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 19:17:32 +0300

    initializating structure lifemoment class

--- src/Lab9/LifeMoment.java
+++ src/Lab9/LifeMoment.java
@@ -1,4 +1,13 @@
 package Lab9;
 
 public class LifeMoment {
+    private final String state;
+
+    public LifeMoment(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
 }
