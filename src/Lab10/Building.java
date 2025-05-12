85e25a675e7c07dd680fb0da3ccabd2086de585a
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 20:52:59 +0300

    initialization building class

--- src/Lab10/Building.java
+++ src/Lab10/Building.java
@@ -1,4 +1,24 @@
 package Lab10;
 
 abstract public class Building {
+    public final void buildHouse() {
+        buildFundament();
+        buildWalls();
+        buildRoof();
+        buildAnotherComponent();
+    }
+
+    private void buildFundament() {
+        System.out.println("Збудовано фундамент");
+    }
+
+    private void buildWalls() {
+        System.out.println("Здубовано стіни");
+    }
+
+    private void buildRoof() {
+        System.out.println("Збудовано дах");
+    }
+
+    protected abstract void buildAnotherComponent();
 }
