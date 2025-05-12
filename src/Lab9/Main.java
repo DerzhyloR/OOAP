abcf39d48dd4d2a6779128de8823faaa9a75af5e
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 19:31:46 +0300

    adding method for integer check

--- src/Lab9/Main.java
+++ src/Lab9/Main.java
@@ -1,4 +1,34 @@
 package Lab9;
 
+import java.util.Scanner;
+
 public class Main {
+    private static Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+        LifePeriod life = new LifePeriod();
+        TimeMachine timeMachine = new TimeMachine();
+
+    }
+
+    public static int getDataInt(String txt,int minOptions){
+        while (true){
+            try {
+                System.out.println(txt);
+                int chyslo=scanner.nextInt();
+                scanner.nextLine();
+                if(chyslo>0 && chyslo<=minOptions){
+                    return chyslo;
+                }
+                else if(chyslo>=0 && minOptions==0){
+                    return chyslo;
+                }
+                else{
+                    System.out.println("Помилка вибору! Повторіть спробу");
+                }
+            }catch (Exception e){
+                System.out.println("ПОМИЛКА ВВОДУ! Повторіть спробу");
+                scanner.next();
+            }
+        }
+    }
 }
