5829673bc250869e67e370cceac57fa887b6db8d
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 20:57:30 +0300

    adding structure of class with attic

--- src/Lab10/HouseAttic.java
+++ src/Lab10/HouseAttic.java
@@ -1,4 +1,8 @@
 package Lab10;
 
-public class HouseAttic {
+public class HouseAttic extends Building{
+    @Override
+    protected void buildAnotherComponent() {
+        System.out.println("Збудовано мансарду");
+    }
 }
