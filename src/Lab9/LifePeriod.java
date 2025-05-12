54b27d2eeb7d7112b0b2c0de063b46ba69e8a1b1
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-12 19:22:57 +0300

    initialization structure of lifeperiod class

--- src/Lab9/LifePeriod.java
+++ src/Lab9/LifePeriod.java
@@ -1,4 +1,24 @@
 package Lab9;
 
 public class LifePeriod {
+    private String state;
+
+    public void setState(String state) {
+        System.out.println("Поточний етап життя: " + state);
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public LifeMoment saveToMemento() {
+        System.out.println("Збережено спогад: " + state);
+        return new LifeMoment(state);
+    }
+
+    public void restoreFromMemento(LifeMoment memonto) {
+        state = memonto.getState();
+        System.out.println("Відновлено етап життя: " + state);
+    }
 }
