ad39dc32fe04b2f0179025f2f0a06ee73ea41023
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-23 00:34:07 +0300

    initialization manager class

--- src/MKP4/Manager.java
+++ src/MKP4/Manager.java
@@ -1,4 +1,15 @@
 package MKP4;
 
-public class Manager {
+class Manager implements Observer {
+    private Game game;
+
+    public Manager(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void update(Player tired) {
+        System.out.println("Менеджер: Гравець " + tired.name + " втомився!");
+        game.substitutePlayer(tired);
+    }
 }
