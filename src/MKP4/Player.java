295c9044066d9d2b83b490a6557d5787cf7cd63a
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-23 00:13:19 +0300

    initialization methods for class player

--- src/MKP4/Player.java
+++ src/MKP4/Player.java
@@ -1,4 +1,25 @@
 package MKP4;
 
-public class Player {
+import java.util.Random;
+
+class Player {
+    String name;
+    int fatigue;
+    public Player(String name) {
+        this.name = name;
+        this.fatigue = 0;
+    }
+
+    public void play() {
+        fatigue += new Random().nextInt(20);
+    }
+
+    public boolean isTired() {
+        return fatigue >= 80;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (Рівень втоми " + fatigue + ")";
+    }
 }
