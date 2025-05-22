5cab85585903f0a843fd1cd50a4e56c4629bd11d
Roman <23derzhylo.r@nltu.lviv.ua>
2025-05-23 00:45:22 +0300

    initialization main class

--- src/MKP4/Main.java
+++ src/MKP4/Main.java
@@ -1,7 +1,24 @@
 package MKP4;
 
-public class Main {
-    public static void main(String [] args){
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+public class Main {
+    public static void main(String[] args) {
+        List<Player> starters = new ArrayList<>();
+        starters.add(new Player("Роман"));
+        starters.add(new Player("Олег"));
+        starters.add(new Player("Mаксим"));
+        starters.add(new Player("Вадим"));
+        Queue<Player> bench = new LinkedList<>();
+        bench.add(new Player("Ігор"));
+        bench.add(new Player("Сергій"));
+        bench.add(new Player("Василь"));
+        Game game = new Game(starters, bench);
+        Manager manager = new Manager(game);
+        game.addObserver(manager);
+        game.simulateMatch();
     }
 }
